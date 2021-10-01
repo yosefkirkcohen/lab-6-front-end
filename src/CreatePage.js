9fefb1d67c99b3de79f800da61160db59cbf1b91
yosefkirkcohen <ycohen45@yahoo.com>
2021-10-01 16:51:13 -0700

    create page

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import request from 'superagent'
 
 export default class CreatePage extends Component {
+    state = {
+        name: '',
+        rating: '',
+        worldchampion: '',
+        image: '',
+    }
+
+    handleNameChange = (e) => {
+        this.setState({name: e.target.value})
+    }
+    handleRatingChange = e => this.setState({rating: e.target.value})
+
     render() {
         return (
             <div>
-                
+                <form>
+                    <label>
+                        Name
+                        <input onChange={this.handleNameChange} />
+                    </label>
+                    <label>
+                        Rating
+                        <input onChange={this.handleRatingChange}/>
+                    </label>
+                    <label>
+                        World Champion?
+                        <input type='radio' value='true'/>
+                        <input type='radio' value='false'/>
+                    </label>
+                </form>
             </div>
         )
     }
